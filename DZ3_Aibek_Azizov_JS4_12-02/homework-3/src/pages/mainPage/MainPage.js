2de3c705eb010152008b61e1c3484ff6cad48b58
Aziz-off <aibek.azizov.e@gmail.com>
2022-05-21 11:11:24 +0600

    fixed menu MainPage

--- DZ3_Aibek_Azizov_JS4_12-02/homework-3/src/pages/mainPage/MainPage.js
+++ DZ3_Aibek_Azizov_JS4_12-02/homework-3/src/pages/mainPage/MainPage.js
@@ -3,24 +3,43 @@ import {Footer} from "../../components/footer/Footer";
 import {Services} from "../../components/services/Services";
 import {Title} from "../../components/title/Title";
 import News from "../../components/news/News";
+import React from "react";
 
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = ({showNews: false});
+        this.newsAppear = this.newsAppear.bind(this);
+    }
 
+    newsAppear () {
+        this.setState({showNews: !this.state.showNews});
+    }
 
-function MainPage() {
-    const servicesMain = ["ux/ui", "front-end", "back-end"];
+    render() {
+        const servicesMain = ["ux/ui", "front-end", "back-end"];
+        return (
+            <div>
+                <Title title="Main Page"/>
+                <Header />
+                <Services services={servicesMain}/>
 
-    return (
-        <div>
-            <Title title="Main Page"/>
-            <Header />
-            <Services services={servicesMain}/>
-            <News />
-            {/*<Footer />*/}
-        </div>
-    )
+                {this.state.showNews === true ? <News /> : ""}
+
+                <button onClick={this.newsAppear} style={{background: "lightblue", padding: "10px", cursor: "pointer"}}>SHOW NEWS</button>
+                {/*<News />*/}
+
+                <Footer />
+            </div>
+        )
+    }
 }
 
 
 
 
+
+
 export default MainPage;
+
+
